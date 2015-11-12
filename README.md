79e36b014fb6dabf48f81798c16691badd234112
Casey Woolley <caseywoolley@gmail.com>
2015-11-11 22:33:30 -0600

    added bookmark popup with star selector

--- README.md
+++ README.md
@@ -1,38 +1,40 @@
 # Starmarks
-====
 
-** Because not all bookmarks are created equal **
+**Because not all bookmarks are created equal**
 
 Starmarks is for anyone who feels like one star isn't enough to say how much you love your bookmarks.  Smartmarks makes it possible to rate bookmarks with 5 stars and track which bookmarks you use the most.  Organize your favorite spots on the web with pression and never worry about losing a great bookmark again!
 
-** About
-```
+## About
+
 Starmarks is a chrome extension for managing bookmarks with ratings
 
-* Sort and organize your websites in new ways *
+**Sort and organize your websites in new ways**
 Create bookmarks with extra meta data
 - 5 star rating
 - last visited
 - visit count
 - tags (coming soon)
 
-** Why Starmarks
-```
+
+## Why Starmarks
+
 If you bookmark a lot of websites it's easy for them to get lost.  Each one may seem special at the time you save it but when every bookmark's special, none of them are.
 
 There needs to be a way to see which bookmarks you like best and visit the most.   Not a big pile you have to sort through to find the good ones.
 
-```
+====
+
 Starmarks let's you bookmark freely.  Save those 'gotta have' websites right along with those 'possibly useful' ones without worrying about them getting mixed up or forgotten.
 
 Save your bookmarks with a star rating from 1 to 5.  Then look them up later by star count, last visited, or times visited.
 
-* "Without Starmarks, bookmarking for me is like throwing my links into the void."" *
+*"Without Starmarks, bookmarking for me is like throwing my links into the void."*
  - The CEO
 
-** Getting Started
-```
+## Getting Started
+
 To use Starmarks in your workflow, just download our chrome extension and click on the 5 star rating icon on any webpage you want to save.
 
-* "I have thousands of bookmarks, from LOLCats to mission critical work resources, I couldn't survive without Starmarks." *
+
+*"I have thousands of bookmarks, from LOLCats to mission critical work resources, I couldn't survive without Starmarks." *
  - Some Guy
--- popup.js
+++ popup.js
@@ -1,130 +1,82 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// Search the bookmarks when entering the search keyword.
-
 $(function() {
-  $('#search').change(function() {
-    debugger
-     $('#bookmarks').empty();
-     dumpBookmarks($('#search').val());
+	var clicked = false;
+  //Save bookmark on star selection
+  $('.star').on('click', function() {
+  	if (!clicked){
+  		clicked = true;
+    	createStarMark($(this).attr('id'));
+  	}
   });
-});
-// Traverse the bookmark tree, and print the folder and nodes.
-function dumpBookmarks(query) {
-  var bookmarkTreeNodes = chrome.bookmarks.getTree(
-    function(bookmarkTreeNodes) {
-      $('#bookmarks').append(dumpTreeNodes(bookmarkTreeNodes, query));
-    });
-}
-function dumpTreeNodes(bookmarkNodes, query) {
-  var list = $('<ul>');
-  var i;
-  for (i = 0; i < bookmarkNodes.length; i++) {
-    list.append(dumpNode(bookmarkNodes[i], query));
-  }
-  return list;
-}
-function dumpNode(bookmarkNode, query) {
-  if (bookmarkNode.title) {
-    if (query && !bookmarkNode.children) {
-      if (String(bookmarkNode.title).indexOf(query) == -1) {
-        return $('<span></span>');
-      }
-    }
-    var anchor = $('<a>');
-    anchor.attr('href', bookmarkNode.url);
-    anchor.text(bookmarkNode.title);
-    /*
-     * When clicking on a bookmark in the extension, a new tab is fired with
-     * the bookmark url.
-     */
-    anchor.click(function() {
-      chrome.tabs.create({url: bookmarkNode.url});
-    });
-    var span = $('<span>');
-    var options = bookmarkNode.children ?
-      $('<span>[<a href="#" id="addlink">Add</a>]</span>') :
-      $('<span>[<a id="editlink" href="#">Edit</a> <a id="deletelink" ' +
-        'href="#">Delete</a>]</span>');
-    var edit = bookmarkNode.children ? $('<table><tr><td>Name</td><td>' +
-      '<input id="title"></td></tr><tr><td>URL</td><td><input id="url">' +
-      '</td></tr></table>') : $('<input>');
-    // Show add and edit links when hover over.
-        span.hover(function() {
-        span.append(options);
-        $('#deletelink').click(function() {
-          $('#deletedialog').empty().dialog({
-                 autoOpen: false,
-                 title: 'Confirm Deletion',
-                 resizable: false,
-                 height: 140,
-                 modal: true,
-                 overlay: {
-                   backgroundColor: '#000',
-                   opacity: 0.5
-                 },
-                 buttons: {
-                   'Yes, Delete It!': function() {
-                      chrome.bookmarks.remove(String(bookmarkNode.id));
-                      span.parent().remove();
-                      $(this).dialog('destroy');
-                    },
-                    Cancel: function() {
-                      $(this).dialog('destroy');
-                    }
-                 }
-               }).dialog('open');
-         });
-        $('#addlink').click(function() {
-          $('#adddialog').empty().append(edit).dialog({autoOpen: false,
-            closeOnEscape: true, title: 'Add New Bookmark', modal: true,
-            buttons: {
-            'Add' : function() {
-               chrome.bookmarks.create({parentId: bookmarkNode.id,
-                 title: $('#title').val(), url: $('#url').val()});
-               $('#bookmarks').empty();
-               $(this).dialog('destroy');
-               window.dumpBookmarks();
-             },
-            'Cancel': function() {
-               $(this).dialog('destroy');
-            }
-          }}).dialog('open');
-        });
-        $('#editlink').click(function() {
-         edit.val(anchor.text());
-         $('#editdialog').empty().append(edit).dialog({autoOpen: false,
-           closeOnEscape: true, title: 'Edit Title', modal: true,
-           show: 'slide', buttons: {
-              'Save': function() {
-                 chrome.bookmarks.update(String(bookmarkNode.id), {
-                   title: edit.val()
-                 });
-                 anchor.text(edit.val());
-                 options.show();
-                 $(this).dialog('destroy');
-              },
-             'Cancel': function() {
-                 $(this).dialog('destroy');
-             }
-         }}).dialog('open');
-        });
-        options.fadeIn();
-      },
-      // unhover
-      function() {
-        options.remove();
-      }).append(anchor);
-  }
-  var li = $(bookmarkNode.title ? '<li>' : '<div>').append(span);
-  if (bookmarkNode.children && bookmarkNode.children.length > 0) {
-    li.append(dumpTreeNodes(bookmarkNode.children, query));
-  }
-  return li;
-}
 
-document.addEventListener('DOMContentLoaded', function () {
-  dumpBookmarks();
+  //console.log bookmark tree
+  chrome.bookmarks.getTree(function(bookmarkTreeNodes) {
+    console.log(bookmarkTreeNodes);
+  });
+
 });
+
+
+var createStarMark = function(rating) {
+  
+  getCurrentTab(function(tab){  
+  	//get bookmark data
+	  var bookmark = {
+	    'parentId': '1',
+	    'title': tab.title,
+	    'url': tab.url
+	  };
+
+  	//get star data
+	  var starData = {
+	    stars: rating,
+	    visits: 0,
+	    lastVisit: Date.now()
+	  };
+	  
+  	//save bookmark
+  	saveBookmark(bookmark);
+  	//save star data
+  	var key = bookmark.url;
+  	saveStarData( bookmark.url, starData );
+  });
+
+};
+
+var getCurrentTab = function(callback) {
+  //query the current tab
+  var currentTab = { 'active': true, 'lastFocusedWindow': true};
+  chrome.tabs.query(currentTab, function(tab) {
+    callback(tab[0]);
+  });
+};
+
+var saveBookmark = function(bookmark) {
+  chrome.bookmarks.create(bookmark, function(newBookmark) {
+    console.log('added: ' + newBookmark.title);
+  });
+};
+
+var saveStarData = function(key, starObj){
+	var starData = {};
+	starData[key] = starObj;
+	console.log(starData);
+	chrome.storage.sync.set( starData, function() {
+    console.log('starData saved');
+  });
+};
+
+var getStarData = function(key){
+  chrome.storage.sync.get( key , function(starData) {
+        // Notify that we saved.
+        console.log(key);
+   console.log(starData);
+  });
+};
+
+
+
+var setDefaultFolder = function() {
+
+};
+  //TODO: save starmark data
+  //TODO: make the icon indicate saved bookmark (w/visit count?)
