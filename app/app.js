205d1f7db739ec6084b1208dddb24b9f73c5b755
Casey Woolley <caseywoolley@gmail.com>
2016-01-20 07:44:28 -0600

    added show edit button on mouse over & whitelisted chrome-extension urls

--- app/app.js
+++ app/app.js
@@ -1,4 +1,12 @@
 angular.module('app.main', ['infinite-scroll', 'angularModalService']);
 angular.module('app.popup', []);
 
-angular.module('app', ['app.main', 'app.popup']);
+angular.module('app', ['app.main', 'app.popup'])
+.config( [
+    '$compileProvider',
+    function( $compileProvider )
+    {   
+        $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|chrome-extension):/);
+        // Angular before v1.2 uses $compileProvider.urlSanitizationWhitelist(...)
+    }
+]);
